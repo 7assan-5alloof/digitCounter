6bd932a3e73f56b137cbf18900d73cbcef2fb4e4
7assan-5alloof <hassankhalloof20#gmail.com>
2020-12-20 12:41:48 +0400

    Write complete program

--- main.cpp
+++ main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+int counter(int);
 int main()
 {
-    cout << "Hello world!" << endl;
+    int number;
+    cout << "Press Ctrl-C stop" << endl;
+    while(true)
+    {
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << counter(number) << endl;
+    }
+
     return 0;
+}
+int counter(int number)
+{
+int numberOfDigits = 0;
+while (number > 0)
+{
+number /= 10;
+numberOfDigits++;
+}
+return numberOfDigits;
 }
